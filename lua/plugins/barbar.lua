77be67c92bd5a001b0d54434868b0ec632366da0
Arnav Kohli <arnavkohli2003@gmail.com>
2024-03-31 19:30:02 +0100

    Add barbar
    Added barbar with status bar at the top.
    
    Signed-off-by: Arnav Kohli <arnavkohli2003@gmail.com>

---
+++ lua/plugins/barbar.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"romgrk/barbar.nvim",
+		init = function()
+			vim.g.barbar_auto_setup = false
+		end,
+		opts = {
+			animation = true,
+			insert_at_start = true,
+		},
+		version = "^1.0.0",
+	},
+}
