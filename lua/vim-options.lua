6c31d06cc8d983d8e2e7e3d6046fd4580661ab62
Arnav Kohli <arnavkohli2003@gmail.com>
2024-04-01 03:00:05 +0100

    Add window switcher
    Added window switcher to default key-bindings
    
    Signed-off-by: Arnav Kohli <arnavkohli2003@gmail.com>

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,4 +1,5 @@
 vim.cmd("set expandtab")
+ -- optional: only update when a new 1.x version is released
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
@@ -9,9 +10,15 @@ vim.o.autoindent = true
 vim.o.scrolloff = 8
 
 vim.api.nvim_set_keymap("v", "<leader>cp", '"+y', { noremap = true })
-vim.api.nvim_exec([[
+vim.api.nvim_exec(
+    [[
   autocmd FileType h,c,javascript setlocal shiftwidth=2
   autocmd FileType h,c,javascript setlocal softtabstop=2
   autocmd FileType h,c,javascript setlocal tabstop=2
-]], false)
+]],
+    false
+)
 
+vim.keymap.set("n", "<C-w>n", ":BufferNext<CR>", { noremap = true })
+
+vim.keymap.set("n", "<C-w>p", ":BufferPrev<CR>", { noremap = true })
